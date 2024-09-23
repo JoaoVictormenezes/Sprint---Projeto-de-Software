57b890aef0c715d0d86bfab316b8998778339d49
Silvstre <124782946+Silvstre@users.noreply.github.com>
2024-09-23 15:52:14 -0300

    Update Tcontato.php

--- view/Tcontato.php
+++ view/Tcontato.php
@@ -10,15 +10,26 @@
 
 <?php require ("../model/conexao.php"); ?>
 
-    <h1> Formulario de contato </h1>
+    <h1> Formulário de Contato </h1>
     <form action="../controler/enviar-email.php" method="POST">
-        <input type="text" name="nome" placeholder="Nome do Contactante">
+        <input type="text" name="nome" placeholder="Nome do Contactante" id="nome">
         <br><br>
-        <input type="text" name="email" placeholder="Email do Contactante">
+        <input type="text" name="email" placeholder="Email do Contactante" id="email">
         <br><br>
-        <input type="text" name="mensagem" placeholder="Mensagem">
+        <input type="text" name="mensagem" placeholder="Mensagem" id="mensagem">
+        <br><br>
         <input type="submit" name="submit" value="Enviar">
+        <button type="button" onclick="limparCampos()">Limpar</button>
+        <a href="../index.html" class="btn btn-default">Voltar</a>
     </form> 
+
+    <script>
+        function limparCampos() {
+            document.getElementById('nome').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('mensagem').value = '';
+        }
+    </script>
 
 </body>
 </html>
